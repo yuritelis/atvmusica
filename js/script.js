249ecb60c948ba2eb91b1b9722c1208249ac3177
2JD <2jd@bglab.local>
2023-09-11 11:37:12 -0300

    NONO COMMIT - Edição de arquivos

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-const canvas = document.getElementById('canvas');
-const perso = canvas.getContext('2d');
+class Perso {
+    constructor(altura, largura, posicao)
+    this.altura = altura;
+    this.largura = largura;
+    this.posicao = posicao;
+}
+
+var canvas = document.getElementById('canvas');
+var perso = canvas.getContext('2d');
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+//personagem do jogador
 perso.beginPath();
 perso.fillStyle = "#53261C";
 perso.fillRect(1700, 450, 110, 80);
 
+//inimigo da parte superior
 perso.beginPath();
 perso.fillStyle = "#391C53";
 perso.fillRect(0, 50, 80, 80);
 
+//inimigo do meio
 perso.beginPath();
 perso.fillStyle = "#391C53";
 perso.fillRect(0, 430, 80, 80);
 
+//inimigo da parte inferior
 perso.beginPath();
 perso.fillStyle = "#391C53";
 perso.fillRect(0, 800, 80, 80);
+
